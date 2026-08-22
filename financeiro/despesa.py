cb7fd89245684dd6551da62f65d11c32256f27b2
IsadoraAlcantara <IsadoraAlca405@gmail.com>
2026-08-21 22:45:52 -0300

    FEAT: criação dos tests da class lancamento

--- financeiro/despesa.py
+++ financeiro/despesa.py
@@ -4,16 +4,8 @@ from financeiro.categoria_despesa import CategoriaDespesa
 
 class Despesa(Lancamento):
 
-    def __init__(self, categoria: CategoriaDespesa, valor: int, date: str) -> None:
-        super().__init__(categoria, valor, date)
+    def __init__(self, categoria: CategoriaDespesa, valor: int, data: str) -> None:
+        super().__init__(categoria, valor, data)
 
     def impacto_no_saldo(self) -> int:
         return -self.valor
-
-    def alterar_valor(self, novo_valor) -> None:
-        if novo_valor <= 0:
-            raise ValueError("O valor de um lançamento não pode ser zero ou negativo")
-        self._valor = novo_valor
-
-    def alterar_categoria(self, nova_categoria: CategoriaDespesa) -> None:
-        self.categoria = nova_categoria
--- financeiro/lancamento.py
+++ financeiro/lancamento.py
@@ -17,8 +17,5 @@ class Lancamento(ABC):
             raise ValueError("O valor de um lançamento não pode ser zero ou negativo")
         self.valor = novo_valor
 
-    def alterar_categoria(self, nova_categoria):
-        if nova_categoria.strip() == "":
-            raise ValueError("O valor de um lançamento não pode ser zero ou negativo")
-        self.valor = nova_categoria
-    # refazer como uma classe em categoria
+    def alterar_categoria(self, nova_categoria: Categoria):
+        self.categoria = nova_categoria
