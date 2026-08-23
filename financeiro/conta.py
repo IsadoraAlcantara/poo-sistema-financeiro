94f34409e039527731b961164d5ac02fa325c036
IsadoraAlcantara <IsadoraAlca405@gmail.com>
2026-08-23 16:22:43 -0300

    FEAT: criação da classe fechamento

--- financeiro/conta.py
+++ financeiro/conta.py
@@ -1,6 +1,11 @@
 from financeiro.lancamento import Lancamento
-from financeiro.categoria_despesa import CategoriaDespesa, NomeCategoriaDespesa, Categoria
+from financeiro.categoria_despesa import (
+    CategoriaDespesa,
+    NomeCategoriaDespesa,
+    Categoria,
+)
 from financeiro.despesa import Despesa
+from financeiro.fechamento import Fechamento
 
 
 class Conta:
@@ -14,19 +19,32 @@ class Conta:
     def calcular_total_lancamentos(self) -> int:
         return sum(i.impacto_no_saldo() for i in self.lancamentos)
 
-    def listar_lancamento_por_categoria(self, categoria_escolhida: Categoria) -> list[Lancamento]:
-        lancamentos_filtados = [i for i in self.lancamentos if i.categoria == categoria_escolhida]
+    def listar_lancamento_por_categoria(
+        self, categoria_escolhida: Categoria
+    ) -> list[Lancamento]:
+        lancamentos_filtados = [
+            i for i in self.lancamentos if i.categoria == categoria_escolhida
+        ]
         return lancamentos_filtados
 
-    def calcular_total_lancamentos_por_categoria(self, categoria_escolhida: Categoria) -> int:
+    def calcular_total_lancamentos_por_categoria(
+        self, categoria_escolhida: Categoria
+    ) -> int:
         lancamentos_filtados = self.listar_lancamento_por_categoria(categoria_escolhida)
         return sum(i.impacto_no_saldo() for i in lancamentos_filtados)
 
     def excedeu_gasto_max(self, categoria_escolhida) -> bool:
         if not isinstance(categoria_escolhida, CategoriaDespesa):
             raise ValueError("Essa categoria não possui gasto máximo")
-        total_gasto = -self.calcular_total_lancamentos_por_categoria(categoria_escolhida)
+        total_gasto = -self.calcular_total_lancamentos_por_categoria(
+            categoria_escolhida
+        )
         return categoria_escolhida.gasto_max < total_gasto
+
+    def fechar_periodo(self, periodo: str) -> Fechamento:
+        fechamento = Fechamento(periodo=periodo, lancamentos=self.lancamentos)
+        self.lancamentos = []
+        return fechamento
 
 
 if __name__ == "__main__":
@@ -35,7 +53,9 @@ if __name__ == "__main__":
     from financeiro.receita import Receita
     from financeiro.categoria_receita import CategoriaReceita, NomeCategoriaReceita
 
-    cat_alimentacao = CategoriaDespesa(nome=NomeCategoriaDespesa.ALIMENTACAO, gasto_max=500)
+    cat_alimentacao = CategoriaDespesa(
+        nome=NomeCategoriaDespesa.ALIMENTACAO, gasto_max=500
+    )
     cat_salario = CategoriaReceita(nome=NomeCategoriaReceita.SALARIO)
 
     despesa1 = Despesa(cat_alimentacao, 300, "2026-08-20")
@@ -50,8 +70,19 @@ if __name__ == "__main__":
     listaLancamentos = conta.listar_lancamento_por_categoria(cat_alimentacao)
 
     for l in listaLancamentos:
-        print(f"Categoria: {l.categoria.nome}, Valor: {l.impacto_no_saldo()}, Data: {l.data}")
+        print(
+            f"Categoria: {l.categoria.nome}, Valor: {l.impacto_no_saldo()}, Data: {l.data}"
+        )
 
     print(f"Valor total conta: {conta.calcular_total_lancamentos()}")
-    print(f"Valor total por categoria: {conta.calcular_total_lancamentos_por_categoria(cat_alimentacao)}")
+    print(
+        f"Valor total por categoria: {conta.calcular_total_lancamentos_por_categoria(cat_alimentacao)}"
+    )
     print(f"Excedeu o gasto máximo? {conta.excedeu_gasto_max(cat_alimentacao)}")
+
+    fechamento_agosto = conta.fechar_periodo("08/2026")
+
+    print(f"Período: {fechamento_agosto.periodo}")
+    print(f"Receitas: R$ {fechamento_agosto.total_receitas}")
+    print(f"Despesas: R$ {fechamento_agosto.total_despesas}")
+    print(f"Saldo Final: R$ {fechamento_agosto.saldo_final}")
